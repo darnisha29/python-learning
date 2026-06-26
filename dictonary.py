47e6cd899abb543a0318353a6543b4ef809348f5
Darnisha Patel <darnisha2910@gmail.com>
2026-06-26 19:07:59 +0530

    Colision Handling In Dictionary Implementation

--- dictonary.py
+++ dictonary.py
@@ -6,13 +6,13 @@
 class HashTable:
     def __init__(self):
         self.max = 100
-        self.arr = [None for i in range(self.max)]
+        self.arr = [ [] for i in range(self.max)]
     
     def get_hash(self,key):
         h = 0
         for char in key:
             h += ord(char)
-        return h%100
+        return h%10
     
     def add(self,key,value):
         h  = self.get_hash(key)
@@ -20,21 +20,40 @@ class HashTable:
         return
     def __setitem__(self,key,value):
         h  = self.get_hash(key)
-        self.arr[h] = value
-        return
+        Found = False
+        for idx,element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][idx] = (key,value)
+                Found = True
+                break
+        if Found == False:
+            self.arr[h].append((key,value))
     
     def get(self,key):
+        print("key is ",key)
         h = self.get_hash(key)
-        return self.arr[h]
+        print("hash is ",h)
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+        print("Not Found")
     
     def __getitem__(self,key):
         h = self.get_hash(key)
-        return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+        print("Not Found")
         
 h1 = HashTable()
-h1.add('9799',"Anjali")
-value = h1.get('9799')
-print(value)
-
-h1['2910']  = "Darnisha"
-print(h1['2910'])
+# h1.add('9799',"Anjali")
+# value = h1.get('9799')
+# print(value)
+print(h1.get_hash('march 6'))
+print(h1.get_hash('march 17'))
+h1['march 6']  = "Darnisha"
+h1['march 17']  = "Khushi"
+h1["march 17"] = "Anjali"
+print(h1['march 17'])
+# print(h1['march 6'])
+print(h1.arr)
