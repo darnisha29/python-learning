d195432048b327b7d21b792881c850ef2d273417
Darnisha Patel <darnisha2910@gmail.com>
2026-07-02 12:32:26 +0530

    Implement graph to print all posible paths

---
+++ graph/graph.py
@@ -0,0 +1,42 @@
+
+class Graph:
+    def __init__(self,edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start , end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        print("graph_dict :: ",self.graph_dict)
+        
+    def get_paths(self,start,end,path=[]):
+        path = path + [start]
+        
+        if start == end:
+            return [path]
+            
+        if start not in self.graph_dict:
+            return []
+            
+        paths = []
+        for node in self.graph_dict[start]:
+            if node  not in path:
+               new_paths =  self.get_paths(node,end,path)
+               for p in new_paths:
+                   paths.append(p)
+                 
+        return paths
+
+if __name__ == '__main__':
+    routes = [
+        ("mumbai","paris"),
+        ("mumbai","dubai"),
+        ("paris","dubai"),
+        ("paris","newyork"),
+        ("dubai","newyork"),
+        ("newyork","torento")
+        ]
+    route_graph = Graph(routes)
+    print("total paths :: ",route_graph.get_paths("mumbai","newyork"))
+    
