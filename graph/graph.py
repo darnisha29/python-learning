e64e5543b61e4310cbaaf0af3cdb1f7e7fff8a27
Darnisha Patel <darnisha2910@gmail.com>
2026-07-02 14:40:47 +0530

    Implement get shorted path function in graph

--- graph/graph.py
+++ graph/graph.py
@@ -27,6 +27,25 @@ class Graph:
                    paths.append(p)
                  
         return paths
+        
+    def get_shortest_path(self,start,end,path=[]):
+        path = path + [start]
+            
+        if start == end:
+            return path
+                
+        if start not in self.graph_dict:
+            return []
+            
+        new_path = []
+        
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node,end,path)
+            if  new_path == [] or len(sp) < len(new_path):
+                new_path = sp
+                    
+        return new_path
 
 if __name__ == '__main__':
     routes = [
@@ -39,4 +58,5 @@ if __name__ == '__main__':
         ]
     route_graph = Graph(routes)
     print("total paths :: ",route_graph.get_paths("mumbai","newyork"))
+    print("shortest path :: ",route_graph.get_shortest_path("mumbai","newyork"))
     
